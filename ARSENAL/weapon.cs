49a4b6298ef9cbe39add9c599be7565b2ff68163
Andreas Thegelström <andreas.thegelstrom@elev.ga.ntig.sem>
2024-10-11 12:51:33 +0200

    Gjorde om hela med ett vapen istället för goomba

---
+++ ARSENAL/weapon.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+public class Warstaff
+{
+    [JsonInclude]
+    public string name;
+
+    [JsonInclude]
+    private int damageMin = 10;
+
+    [JsonInclude]
+    private int damageMax = 30;
+
+    public int Meteor()
+    {
+        return Random.Shared.Next(damageMin, damageMax);
+    }
+}
